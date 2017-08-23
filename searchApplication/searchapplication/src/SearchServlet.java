7451d1af3753a7268c79f2e383ecd7b22d194063
trainee <trainee@190617-pc-la1>
2017-08-23 16:02:50 +0530

    search application

---
+++ searchApplication/searchapplication/src/SearchServlet.java
@@ -0,0 +1,36 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class SearchServlet
+ */
+public class SearchServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public SearchServlet() {
+    	super();
+}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		String data = request.getParameter("info");
+		response.sendRedirect("https://www.google.co.in/#q="+data);
+		
+		
+	}
+
+}
